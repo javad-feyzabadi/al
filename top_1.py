a9dedfb70f1ad837fa73736f3dd11277f403015b
javad <javad.feyzabadi@yahoo.com>
2023-04-09 16:11:46 +0330

    top1

--- top_1.py
+++ top_1.py
@@ -1,18 +1,21 @@
 def top(arr):
-    value = {}
+    values = {}
     result = []
     f_val = 0
 
     for i in arr:
-        if i in value:
-            value[i] += 1 
-        else :
-            value[i] = 1
+        if i in values:
+            values[i] += 1 
+        else:
+            values[i] = 1
 
-    f_val = max(value.values())
-    for i in value.keys:
-        if value[i] == f_val:
+    f_val = max(values.values())
+
+    for i in values.keys():
+        if values[i] == f_val:
             result.append(i)
         else:
             continue
     return result
+
+print(top([1,2,1,3,4,2]))
