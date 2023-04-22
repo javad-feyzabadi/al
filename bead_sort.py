39c79ae98765542cbf1141da7a21ff3b21a1f84a
javad <javad.feyzabadi@yahoo.com>
2023-04-22 15:27:27 +0330

    bead_sort

--- bead_sort.py
+++ bead_sort.py
@@ -1,7 +1,7 @@
 def bead_sort(sequence):
     """
-    bead_sort([4,7,2,9,3,6,7])
-    >>> [2,3,4,6,7,7,9]
+    >>> bead_sort([4,7,2,9,3,6,7])
+    [2,3,4,6,7,7,9]
     """
     if any(not isinstance(x,int) or x < 0 for x in sequence):
         raise TypeError('sequence must be list of non-negative integers')
