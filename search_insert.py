a5944e110059f8d45b93e746d9223c664b34e603
javad <javad.feyzabadi@yahoo.com>
2023-04-22 15:30:17 +0330

    search_insert

--- search_insert.py
+++ search_insert.py
@@ -1,14 +1,11 @@
 def search_insert(arr,val):
     """
-    arr = [1,3,5,6]
-    val = 3
-    >>> 1
-    arr = [1,3,5,6]
-    val = 7
-    >>> 4
-    arr = [1,3,5,6]
-    val = 0
-    >>> 0
+    >>> search_insert([1,3,5,6],3)
+    1
+    >>> search_insert([1,3,5,6],7)
+    4
+    >>> search_insert([1,3,5,6],0)
+    0
     """
 
     low = 0
