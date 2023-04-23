5eefaa2e111b8d33322da8d4fed7c01b94447890
javad <javad.feyzabadi@yahoo.com>
2023-04-24 00:54:49 +0330

    rotate

--- rotate.py
+++ rotate.py
@@ -5,8 +5,8 @@ def rotate(s,k):
     """
     double_s = s + s
     if k <= len(s):
-        return double_s[k:k+len(s)]
+        return double_s[k : k+len(s)]
     else:
-        return double_s[k - len(s):k]
+        return double_s[k - len(s) : k]
     
 print(rotate('hello',7))
