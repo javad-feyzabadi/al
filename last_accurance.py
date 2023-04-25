30f02beec19ba92e72b7e8e952b99e40727333d2
javad <javad.feyzabadi@yahoo.com>
2023-04-25 18:02:28 +0330

    last_accurance

---
+++ last_accurance.py
@@ -0,0 +1,17 @@
+def last_accurance(array,element):
+    """
+    >>> last_accurance([2,2,2,3,3,4,4,5,5,5],5)
+    9
+    """
+    low, high = 0 , len(array)-1
+    while low <= high :
+        mid = (high + low) // 2
+        if (array[mid] == element and mid == len(array)-1) or \
+        (array[mid] == element and array[mid+1] > element):
+            return mid
+        elif (array[mid] <= element):
+            low = mid + 1 
+        else:
+            high = mid - 1
+
+print(last_accurance([2,2,2,3,3,4,4,5,5,5],5))
