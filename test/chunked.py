da4477821b4f178e29e547c819a2f4b8cc9bf3ce
javad <javad.feyzabadi@yahoo.com>
2023-04-26 16:40:43 +0330

    chunked

---
+++ test/chunked.py
@@ -0,0 +1,23 @@
+from itertools import islice
+from functools import partial
+
+l = [1,2,3,4,5,6,7]
+
+def take(iterable,n):
+    return list(islice(iterable,n))
+
+def chunked(iterable,n,strict = False):
+    iterator = iter(partial(take,iter(iterable),n),[])
+    if strict:
+        if n is None:
+            raise ValueError('n cant be None when strict is True')
+        def ret():
+            for chunk in iterator:
+                if len(chunk) != n :
+                    raise ValueError('iterator is not divisible by n')
+                yield chunk
+        return iter(ret())
+    else:
+        return iterator
+    
+print(list(chunked(l,3,strict=True)))
