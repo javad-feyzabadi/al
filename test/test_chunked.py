dabf23bad790295db7ce7e2f6e8c7f25ef97fe90
javad <javad.feyzabadi@yahoo.com>
2023-04-26 18:06:58 +0330

    test_chunked

--- test/test_chunked.py
+++ test/test_chunked.py
@@ -17,3 +17,44 @@ class TakeTests(TestCase):
     def test_take_too_much(self):
         t = chunked.take(range(5),10)
         self.assertEqual(t,[0,1,2,3,4])
+
+
+class ChunkedTests(TestCase):
+    
+    def test_even(self):
+        self.assertEqual(
+            list(chunked.chunked('ABCDEF',3)),[['A','B','C'],['D','E','F']]
+        )
+
+    def test_odd(self):
+        self.assertEqual(
+            list(chunked.chunked('ABCDE',3)),[['A','B','C'],['D','E']]
+        )
+
+    def test_none(self):
+        self.assertEqual(
+            list(chunked.chunked('ABCDE',None)),[['A','B','C','D','E']]
+        )
+
+    def test_strict_false(self):
+        self.assertEqual(
+            list(chunked.chunked('ABCDE',3,strict=False)),[['A','B','C'],['D','E']]
+        )
+
+    def test_strict_true(self):
+        def f():
+            return list(chunked.chunked('ABCDE',3,strict=True))
+        self.assertRaisesRegex(
+            ValueError,'iterator is not divisible by n',f
+        )
+        self.assertEqual(
+            list(chunked.chunked('ABCDEF',None,strict=True)),
+            [['A','B','C'],['D','E','F']]
+        )
+
+    def test_strict_true_size_none(self):
+        def f():
+            return list(chunked.chunked('ABCDE',None,strict=True))
+        self.assertRaisesRegex(
+            ValueError,'n cant be None when strict is True',f
+        )
