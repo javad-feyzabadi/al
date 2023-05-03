cd716639eb299e266ef1e3294d682f405f131152
javad <javad.feyzabadi@yahoo.com>
2023-05-03 13:05:02 +0330

    create first.py

---
+++ test/first.py
@@ -0,0 +1,18 @@
+
+l = [1,2,3,4,5,6]
+a = []
+_marker = object()
+def first(iterable,default=_marker):
+    try:
+        return next(iter(iterable))
+    except StopIteration as e:
+        if default is _marker:
+            raise ValueError('first() was called on an emty iterable,'
+                             'and no default value was provided') from e
+        return default
+
+print(first(a,'boooooooo'))
+print(40 * "*")
+print(first(l))
+print(40 * "*")
+print(first(a,))
